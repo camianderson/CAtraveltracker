600b72f58f89df12a5694887618f13e0ef13260e
Cami Anderson <camidantas@icloud.com>
2022-06-07 15:20:25 -0400

    Add classes for traveler and trip

---
+++ src/Trip.js
@@ -0,0 +1,15 @@
+class Trip{
+    constructor(){
+        this.id = tripID;
+        this.userID = id;
+        this.destinationID = destinationID;
+        this.travelers = travelers;
+        this.date = date;
+        this.duration = duration;
+        this.status = 'pending';
+        this.suggestedActivities = [];
+
+    }
+}
+
+export default Trip;
